65870230dbce0806643f8b65b6bcc7c6ce56c155
Rafael Velloso <rafavelloso2@gmail.com>
2022-01-15 01:02:34 -0300

    Create pedra_papel_tesoura.py
    game.py

---
+++ pedra_papel_tesoura.py
@@ -0,0 +1,51 @@
+from random import randint
+from time import sleep
+
+item = ('Pedra', 'Papel', 'Tesoura')
+pc = randint(0, 2)
+print('''SUAS OPÇÕES
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!!')
+
+print('-=' * 11)
+
+print('Computador jogou {}'.format(item[pc]))
+print('Jogador jogou {}'.format(item[jogador]))
+
+print('-=' * 11)
+
+if pc == 0:      #computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif pc == 1:    #computador jogou PAPEL
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif pc == 2:    #computador jogou TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
